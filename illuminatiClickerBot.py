9368d5d16d494c43e6b4ca08c9bbc6ead5aab9b4
Truite Séchée <66438253+Truiteseche@users.noreply.github.com>
2022-07-15 19:57:41 +0200

    Update illuminatiClickerBot.py
    More precise "How to use" pop-up
    Forever loop
    Import keyboard module
    User input management :
    -> "alt" : play/pause the process
    -> "space" : stop the process
    Keyboard module error management

--- illuminatiClickerBot.py
+++ illuminatiClickerBot.py
@@ -1,17 +1,37 @@
 
 import pyautogui
 import time
+import keyboard
 
 time.sleep(1)
-userInput = pyautogui.confirm("This is a bot for ILLUMINATI CLICKER© v1.19 by Nickro01 :"
-                "\n1. Follow this link : https://scratch.mit.edu/projects/493520075/"
+userInput = pyautogui.confirm("Welcome to illuminatiClickerBot, a bot made for ILLUMINATI CLICKER© v1.19 by Nickro_01\nFolow the procedure :"
+                "\n1. Visit this link : https://scratch.mit.edu/projects/493520075/"
                 "\n2. Put the project in fullscreen"
                 "\n3. Launch the project by clicking on the green flag"
-                "\n4. Press 'OK' to run the script")
+                "\n4. Press 'OK' to run the script"
+                "\n5. Place your mouse on the illuminati triangle"
+                "\nThe bot click automatically. Press 'alt' button to pause/launch the process, press 'space' to stop the process...")
 
 if userInput == "OK":
     #pyautogui
     screenWidth, screenHeight = pyautogui.size()
     pyautogui.moveTo(screenWidth/2, screenHeight/2)
     botRunning = True
-    pyautogui.position(screenWidth/2, screenHeight/2)
+    botMustClick = True
+    while botRunning:
+        if botMustClick:
+            pyautogui.doubleClick()
+            time.sleep(1 / 25)
+
+        try:
+            if keyboard.is_pressed('alt'):
+                if botMustClick:
+                    botMustClick = False
+                else:
+                    botMustClick = True
+                while keyboard.is_pressed('alt'):
+                    time.sleep(0)
+            if keyboard.is_pressed('space'):
+                break
+        except:
+            break
